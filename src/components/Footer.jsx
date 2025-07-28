9e924960a6ac0431dd5ffebe62cd2fd763baa9b0
vkassingh <44vikasrajput44@gmail.com>
2025-07-28 17:09:36 +0530

    Created seperate tables for fee structure based on domain, Added search and filter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
 
   return (
-    <footer className="bg-black text-white py-8">
+    <footer className="bg-gradient-to-r from-blue-800 via-blue-700 to-blue-700 text-white py-8">
       <div className="container mx-auto px-6 lg:px-8 ">
         <div className="grid grid-cols-1 md:grid-cols-2 py-4 lg:grid-cols-5 gap-8 ">
           {/* University Info */}
